f8a945cf94d56acf333940844decc6ebd83cb40b
soramame0256 <62879324+soramame0256@users.noreply.github.com>
2024-01-08 08:44:40 +0900

    fix: it was c&p coded

--- src/main/java/com/github/soramame0256/beanpunishments/EventListener.java
+++ src/main/java/com/github/soramame0256/beanpunishments/EventListener.java
@@ -27,9 +27,9 @@ public class EventListener implements Listener {
     public void onChat(AsyncPlayerChatEvent e){
         PunishmentManager pm=BeanPunishments.getPunishmentManager();
         if(pm.isMuted(e.getPlayer())){
-            PunishmentManager.BanStatus bs = pm.getBanStatus(e.getPlayer());
-            String timeStamp = bs.getEnd() > 999999999 ? (bs.getEnd()==Long.MAX_VALUE ? "permanent" : "effective permanent") : Timestamp.from(Instant.ofEpochSecond(bs.getEnd())).toString();
-            if(bs.getEnd()==Long.MAX_VALUE){
+            PunishmentManager.MuteStatus ms = pm.getMuteStatus(e.getPlayer());
+            String timeStamp = ms.getEnd() > 999999999 ? (ms.getEnd()==Long.MAX_VALUE ? "permanent" : "effective permanent") : Timestamp.from(Instant.ofEpochSecond(ms.getEnd())).toString();
+            if(ms.getEnd()==Long.MAX_VALUE){
                 ChatUtils.messageTranslated(e.getPlayer(), "punish.muted.permanent");
             }else{
                 ChatUtils.messageTranslated(e.getPlayer(), "punish.muted", timeStamp);
